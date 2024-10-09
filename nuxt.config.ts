aa85e25d4f128dec239afc513b9af558ad65ac2f
Adrian Latorre <adri.latorre@gmail.com>
2024-10-09 10:46:40 +0000

    FIX: Fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,20 @@
 export default defineNuxtConfig({
-  head: {
-    meta: [
-      { name: 'theme-color', content: '#ffffff' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-      { rel: "manifest", href: "/site.webmanifest" },
-      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }
-    ],
+  app: {
+    head: {
+      meta: [
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" }
+      ],
+    },
   },
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   css: ['~/assets/css/main.css'],
 
